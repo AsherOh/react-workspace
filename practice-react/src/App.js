672d36513ff172fcd4d34e6c4d6a98895614a8ab
AsherOh <102159721+AsherOh@users.noreply.github.com>
2022-10-05 02:26:54 +0900

    remove LF error

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -6,7 +6,7 @@ function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
-    <ThemeContext.Provider value={isLoggedIn}>
+    <ThemeContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
       <div className="App">
         <button>Clicked</button>
       </div>
