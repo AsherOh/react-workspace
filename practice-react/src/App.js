9d8e3ad353b31c3e51448ba4b483976de36a1a56
AsherOh <102159721+AsherOh@users.noreply.github.com>
2022-10-05 02:23:25 +0900

    useContext

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from "react";
 import AppRouter from "./components/AppRouter";
-
+import { ThemeContext } from "./context/ThemeContext";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
   return (
-    <div className="App">
-      <button>Clicked</button>
-    </div>
+    <ThemeContext.Provider value={isLoggedIn}>
+      <div className="App">
+        <button>Clicked</button>
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
