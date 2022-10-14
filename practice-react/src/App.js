6869cc3a648f0a3eaee42971f0b70143e8be4940
AsherOh <102159721+AsherOh@users.noreply.github.com>
2022-10-14 18:39:33 +0900

    add comment

--- practice-react/src/App.js
+++ practice-react/src/App.js
@@ -9,6 +9,8 @@ function App() {
     <ThemeContext.Provider value={{ isLoggedIn, setIsLoggedIn }}>
       <div className="App">
         <button>Clicked</button>
+        <p>Hi</p>
+        <button>Hello</button>
       </div>
     </ThemeContext.Provider>
   );
