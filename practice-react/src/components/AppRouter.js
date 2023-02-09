f341a3e7c80b3f58356a62a9c7003f989c77e288
AsherOh <102159721+AsherOh@users.noreply.github.com>
2023-02-09 20:20:09 +0900

    style: adjust the tab space

--- practice-react/src/components/AppRouter.js
+++ practice-react/src/components/AppRouter.js
@@ -1,15 +1,13 @@
-import React from 'react';
+import React from "react";
 
-const AppRouter = ({ isLoggedIn, setIsLoggedIn }) => {
-    return (
-        <>
-            <Router>
-                <Route>
-                    
-                </Route>
-            </Router>
-        </>
-    );
+const AppRouter = () => {
+  return (
+    <>
+      <Router>
+        <Route></Route>
+      </Router>
+    </>
+  );
 };
 
 export default AppRouter;
